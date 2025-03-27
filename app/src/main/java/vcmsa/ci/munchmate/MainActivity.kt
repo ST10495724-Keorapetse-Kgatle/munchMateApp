0b121d5df66ea1ec87d356e2e3acee84c0ca06ec
lab_services_student <ST10495724@vcconnect.edu.za>
2025-03-27 16:42:05 +0200

    added my meal option when the user types in morning

--- app/src/main/java/vcmsa/ci/munchmate/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/munchmate/MainActivity.kt
@@ -43,12 +43,38 @@ class MainActivity : AppCompatActivity() {
             //Clear the displayOpt text
             displayOpt.text = "" // Clear the displayOpt text
         }
+    }
 
+    // Set up the munch button listener
+        val timeInput = timeOfDay.text.toString().lowercase().trim()
+        val mealOption = when (timeInput) {
+            //when the user enters morning
+            "morning" -> "Avocado toast "
+            //when the user enters mid-morning
+            "mid-morning snack" -> "a greek yogurt parfait"
+            //when the user enters afternoon
+            "afternoon" -> "Chicken Caesar Wrap"
+            //when the user enters afternoon snack
+            "afternoon snack " -> "chips and dip"
+            //when the user enters dinner
+            " dinner" -> "Spaghetti Bolognese"
+            //when the user enters after dinner snack
+            "after dinner snack " -> "warm milk,crackers and cheese"
+            //when the user enters dinner
+            "desert" -> "tiramisu"
+            // if the user enters anything else
+            else -> "Invalid Time Of day Please Enter Valid Time In Words!"
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
     }
 }
+
+
+
+       // ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+          //  val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+           // v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+           //insets
+      //  }
+   // }
+//}
