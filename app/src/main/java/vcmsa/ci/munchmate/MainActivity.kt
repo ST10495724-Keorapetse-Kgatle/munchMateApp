956fe93fe8282e87075a76abf0aa8b644f7eab7c
lab_services_student <ST10495724@vcconnect.edu.za>
2025-03-27 16:29:41 +0200

    declared my variables

--- app/src/main/java/vcmsa/ci/munchmate/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/munchmate/MainActivity.kt
@@ -1,12 +1,23 @@
 package vcmsa.ci.munchmate
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
+
+    // Declaration
+    private lateinit var timeOfDay: TextView
+    private lateinit var displayOpt: TextView
+    private lateinit var btnMunchTime: Button
+    private lateinit var btnExit: Button
+    private lateinit var btnReset: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
