6533b04023bc08a8f1d7a34eefea11272e79df5c
lab_services_student <ST10495724@vcconnect.edu.za>
2025-03-27 17:19:53 +0200

    fixed my errors

--- app/src/main/java/vcmsa/ci/munchmate/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/munchmate/MainActivity.kt
@@ -5,9 +5,6 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,12 +27,10 @@ class MainActivity : AppCompatActivity() {
         btnExit = findViewById(R.id.btnExit)
         btnReset = findViewById(R.id.btnReset)
 
-
         // Exit the application
         btnExit.setOnClickListener {
             finish()
         }
-
         // Reset button
         btnReset.setOnClickListener {
             // it  clear the timeOfDay text
@@ -43,38 +38,29 @@ class MainActivity : AppCompatActivity() {
             //Clear the displayOpt text
             displayOpt.text = "" // Clear the displayOpt text
         }
-    }
-
-    // Set up the munch button listener
-        val timeInput = timeOfDay.text.toString().lowercase().trim()
-        val mealOption = when (timeInput) {
-            //when the user enters morning
-            "morning" -> "Avocado toast "
-            //when the user enters mid-morning
-            "mid-morning snack" -> "a greek yogurt parfait"
-            //when the user enters afternoon
-            "afternoon" -> "Chicken Caesar Wrap"
-            //when the user enters afternoon snack
-            "afternoon snack " -> "chips and dip"
-            //when the user enters dinner
-            " dinner" -> "Spaghetti Bolognese"
-            //when the user enters after dinner snack
-            "after dinner snack " -> "warm milk,crackers and cheese"
-            //when the user enters dinner
-            "desert" -> "tiramisu"
-            // if the user enters anything else
-            else -> "Invalid Time Of day Please Enter Valid Time In Words!"
-
+        // Set up the munch button listener
+        btnMunchTime.setOnClickListener {
+            val timeInput = timeOfDay.text.toString().lowercase().trim()
+            val mealOption = when (timeInput) {
+                //when the user enters morning
+                "morning" -> "Avocado toast "
+                //when the user enters mid-morning
+                "mid-morning snack"-> "a greek yogurt parfait"
+                //when the user enters afternoon
+                "afternoon" -> "Chicken Caesar Wrap"
+                //when the user enters afternoon snack
+                "afternoon snack "-> "chips and dip"
+                //when the user enters dinner
+                " dinner" -> "Spaghetti Bolognese"
+                //when the user enters after dinner snack
+                "after dinner snack "-> "warm milk,crackers and cheese"
+                //when the user enters dinner
+                "desert" -> "tiramisu"
+                // if the user enters anything else
+                else -> "Invalid Time Of day Please Enter Valid Time In Words!"
+            }
+            //initialising so the mealOption should be displayed on the textView displayOpt
+            displayOpt.text = mealOption
         }
     }
 }
-
-
-
-       // ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-          //  val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-           // v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-           //insets
-      //  }
-   // }
-//}
